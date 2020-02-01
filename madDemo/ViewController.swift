a313a00bd6f620575254420912540b53f3fbb33f
miramookerjee8000@gmail.com <miramookerjee8000@gmail.com>
2020-02-01 01:25:18 -0500

    initial commit

--- madDemo/ViewController.swift
+++ madDemo/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var textLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonPress(_ sender: Any) {
+        print("Hello")
+        textLabel.textColor = UIColor.orange
+    }
 }
 
